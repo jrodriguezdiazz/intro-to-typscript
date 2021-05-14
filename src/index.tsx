c0d398e8ef3cf0884dc37f8ca85205f74516593c
jrodriguezdiazz <jorgedejesusrodriguezdiaz@gmail.com>
2021-05-14 12:28:38 -0400

    Setup the project with TS

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import "react-app-polyfill/ie11";
+// tslint:disable-next-line
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./components/App";
+import "./index.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
